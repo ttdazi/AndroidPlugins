cefce93ade3be0324851ed94b549600290d46150
yuanqing <ttaidazi@gmail.com>
2021-08-05 00:55:09 +0800

    更新：RouterTransform.groovy

--- routePlugin/src/main/groovy/com/ttdazi/gradle/PluginRoute.groovy
+++ routePlugin/src/main/groovy/com/ttdazi/gradle/PluginRoute.groovy
@@ -1,7 +1,9 @@
 package com.ttdazi.gradle
+
 import com.android.build.gradle.AppExtension
+import com.android.build.gradle.AppPlugin
 import com.ttdazi.gradle.model.ModelExtension
-import com.ttdazi.gradle.transform.AspectJTransform
+import com.ttdazi.gradle.transform.RouterTransform
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -9,11 +11,12 @@ class PluginRoute implements Plugin<Project> {
     @Override
     void apply(Project project) {
         ModelExtension extension = project.extensions.create("modelName", ModelExtension)
-        println("------------ apply  model extension --------------" +  extension.modelName)
         boolean hasApp = project.getPlugins().hasPlugin(AppPlugin.class)
+        println("$hasApp------------ apply  model extension --------------" + extension.modelName)
         if (hasApp) {
             AppExtension appExtension = project.getExtensions().getByType(AppExtension.class)
-            appExtension.registerTransform(new AspectJTransform(), Collections.EMPTY_LIST)
+            appExtension.registerTransform(new RouterTransform(project), Collections.EMPTY_LIST)
+            //注册日志统计transform，统计的transform。美团的热更新Roubust也是在这自定义的transform。
         }
     }
 
